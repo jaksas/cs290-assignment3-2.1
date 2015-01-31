9d3fd15e380e548552cf98e0e485e4972634cb96
jaksas <jaksas@onid.oregonstate.edu>
2015-01-31 17:55:04 -0600

    Finally able to access the languages of each file, huzzah

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,3 @@
-var noLangs = new Array(); 
-
 function valNum () {
 	if (document.getElementsByName("number_pgs_chc")[0].value < 1) {
 		document.getElementsByName("number_pgs_chc")[0].value = 1;
@@ -10,33 +8,71 @@ function valNum () {
 }
 
 function userPrefs() {
+	var noLangs = new Array(); 
+	
+	//Store the number of pages to be shown in session storage
 	sessionStorage["number_pgs"] = document.getElementsByName('number_pgs_chc')[0].value;
+	
+	//Clear the filter and unwanted language array from the previous search 
 	sessionStorage.removeItem("noLanguages");
 	noLangs = []; 
+	
+	//Set the language filter
 	for(var i = 0; i < 4; i++) {
 		if (document.getElementsByName("language_filter")[i].checked) {
 			noLangs.push(document.getElementsByName('language_filter')[i].value);
 		}
 	}
+	
+	//Store the language filter 
 	sessionStorage["noLanguages"] = JSON.stringify(noLangs); 
 }
 	
 function getGists() {
 	var req = new XMLHttpRequest();
+	var gistsArray;
+	var url = "https://api.github.com/gists/public";
+	
 	if(!req) {
 		throw 'Unable to create HttpRequest.';
 	}
-	var url = "https://api.github.com/gists/public";
 	
 	req.open("GET", url, true);
 	
 	req.onreadystatechange=function() 
 	{
 		if(this.readyState==4) {
-			document.getElementById('showGists').innerHTML = req.responseText; 
+			gistsArray = JSON.parse(req.responseText); 
+			filterGists(gistsArray); 
 		}
 	}
 	
 	req.send(null); 	
 }
 	
+function filterGists(gistsArray) {
+	var printableGists = new Array(); 
+	var noLangs = JSON.parse(sessionStorage.noLanguages); 
+	var dummy; 
+	
+	for (var i = 0; i < gistsArray.length; i++) { 
+		
+		for(var key in gistsArray[i].files) {
+			console.log(key);
+			console.log(gistsArray[i].files[key]["language"]);
+		}
+		
+		//console.log(gistsArray[i].files); 
+		//console.log(gistsArray[i].files[0]); 
+		
+		for (var j = 0; j < noLangs.length; j++) {
+			
+			if (gistsArray[i].files.language == noLangs[i])
+			{
+				printableGists.push([gistsArray[i].url, gistsArray[i].description]); 
+				//console.log(gistsArray[i].url);
+				//console.log(gistsArray[i].files.language);
+			}	
+		}
+	}
+}
